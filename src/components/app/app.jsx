88aedf960e6ac1a7727edb3a595eb01ced1d7f5f
vvikota <rymsha.viktor@gmail.com>
2020-05-26 15:37:27 +0300

    add App component

---
+++ src/components/app/app.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {userName} = props;
+
+  return (
+    <>
+
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link header__logo-link--active">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+              </a>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a className="header__nav-link header__nav-link--profile" href="#">
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                    <span className="header__user-name user__name">{userName}</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--index page__main--index-empty">
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="cities tabs">
+          <section className="locations container">
+            <ul className="locations__list tabs__list">
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Paris</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Cologne</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Brussels</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item">
+                  <span>Amsterdam</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Hamburg</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item tabs__item--active" href="#">
+                  <span>Dusseldorf</span>
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
+        <div className="cities__places-wrapper">
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">No places to stay available</b>
+                <p className="cities__status-description">We could not find any property availbale at the moment in Dusseldorf</p>
+              </div>
+            </section>
+            <div className="cities__right-section">
+            </div>
+          </div>
+        </div>
+      </main>;
+
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Hello, world!`;
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const init = () => {
+  const settings = {
+    userName: `vik792@gmail.com`,
+  };
+
+  ReactDOM.render(
+      <App
+        userName={settings.userName}
+      />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
