871728a98ef003892d2407a06002507d4f5afde7
vvikota <rymsha.viktor@gmail.com>
2020-05-28 00:39:51 +0300

    fix tests

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,22 +3,14 @@ import renderer from 'react-test-renderer';
 import App from './app.jsx';
 
 const mock = {
-  cardTitles: [
-    `Beautiful & luxurious apartment at great location`,
-    `Wood and stone place`,
-    `Canal View Prinsengracht`,
-    `Nice, cozy, warm big bed apartment`,
-    `Wood and stone place`
-  ],
   userName: `vik792@gmail.com`,
 };
 
 it(`App is correcctly render`, ()=> {
 
   const tree = renderer.create(<App
-    cardTitles={mock.cardTitles}
     userName={mock.userName}
-    openCard={jest.fn()}
+    offers={[]}
   />).toJSON();
 
   expect(tree).toMatchSnapshot();
--- src/components/offer-card/offer-card.e2e.test.js
+++ src/components/offer-card/offer-card.e2e.test.js
@@ -1,4 +1,9 @@
-export default [
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import OfferCard from './offer-card.jsx';
+
+const mock = [
   {
     premium: true,
     price: 120,
@@ -40,3 +45,19 @@ export default [
     placeDiscription: `Wood and stone place`
   },
 ];
+
+Enzyme.configure({adapter: new Adapter()});
+
+it(`Click on card-title, correctly works`, () => {
+  const clickHandler = jest.fn();
+  const app = shallow(<OfferCard
+    offers={mock}
+    openCard={clickHandler}
+  />);
+
+  const openCardButtons = app.find(`.place-card__name-link`);
+  const openCardButtonsCount = openCardButtons.length;
+  openCardButtons.forEach((it) => it.simulate(`click`, {preventDefault() {}}));
+
+  expect(clickHandler).toHaveBeenCalledTimes(openCardButtonsCount);
+});
