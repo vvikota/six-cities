0e64499cedce3361168198613648a11a9ec6c0b8
vvikota <rymsha.viktor@gmail.com>
2020-06-15 22:59:50 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
 import App from "./components/app/app.jsx";
 import offers from "./mocks/offers.js";
+import settings from "./mocks/settings.js";
+import {reducer} from "./reducer.js";
 
-const init = () => {
-  const settings = {
-    userName: `vik792@gmail.com`,
-  };
+const init = (appOffers, appSettings) => {
+  const store = createStore(reducer);
 
-
-  ReactDOM.render(
-      <App
-        userName={settings.userName}
-        offers={offers}
-        openCard={(offer) => {
-          // eslint-disable-next-line no-console
-          console.log(offer);
-        }}
-      />,
-      document.querySelector(`#root`)
+  ReactDOM.render(<Provider store={store}>
+    <App
+      userName={appSettings.userName}
+      offers={appOffers}
+      openCard={(offer) => {
+        // eslint-disable-next-line no-console
+        console.log(offer);
+      }}
+    />
+  </Provider>,
+  document.querySelector(`#root`)
   );
 };
 
-init();
+init(offers, settings);
+
---
+++ src/mocks/settings.js
@@ -0,0 +1,3 @@
+export default {
+  userName: `vik792@gmail.com`,
+};
